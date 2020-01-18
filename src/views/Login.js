0333e1b56197d596bc8d755f5be96bba048ed171
dragon081223 <mantine08@gmail.com>
2020-01-17 19:54:01 -0800

    Update Login.js

--- src/views/Login.js
+++ src/views/Login.js
@@ -4,16 +4,8 @@ import LoginComponent from "../components/LoginComponent";
 class Login extends React.Component {
   render() {
     return (
-      <div
-        style={{
-          background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
-        }}
-      >
-        <LoginComponent
-          style={{
-            background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
-          }}
-        />
+      <div>
+        <LoginComponent />
       </div>
     );
   }
